7cdde57e3a2fa929e684fe5a66ea568c171134ab
Andrey Yurovsky <andrey.yurovsky@convergeddevices.net>
2011-01-26 07:41:53 -0800

    scan-i2c: make output a little nicer
    also check for i2c_dev driver

--- scan-i2c.sh
+++ scan-i2c.sh
@@ -1,16 +1,23 @@
-#!/bin/sh
+#!/bin/bash
 
 if [ $# -lt 1 ]; then
 	echo "usage: $0 <busnum>"
 	exit 1
 fi
 
-if [ ! -c /dev/i2c-$1 ]; then
-	echo "invalid bus number"
+if [ ! -e /sys/module/i2c_dev ]; then
+	echo "load the i2c_dev driver first"
 	exit 2
 fi
 
+if [ ! -c /dev/i2c-$1 ]; then
+	echo "invalid bus number"
+	exit 3
+fi
+
 for i in `seq 1 63`; do
-	printf "%x\n" $i
-	i2c-ctl /dev/i2c-$1 rb `printf "%x" $i` 0 1
+	i2c-ctl /dev/i2c-$1 rb `printf "%x" $i` 0 1 &>/dev/null
+	if [ "$?" = "0" ]; then
+		printf "device at 0x%x\n" $i
+	fi
 done
